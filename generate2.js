55c000d5984878305c3a6da669edcaeb3ce09c66
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2025-06-13 10:40:38 +0200

    Create generate2.js

---
+++ generate2.js
@@ -0,0 +1,26 @@
+const generate = require('@retrogen/generate')
+const fs = require('fs/promises')
+const path = require('path')
+const { DateTime } = require('luxon')
+const generate = require('@retrogen/generate')
+const fs = require('fs/promises')
+async function markdown () {
+  const organization = 'vbcq-volley' // change this if you want to use a different organization
+
+  const now = DateTime.now()
+  const then = now.minus({ days: 1 }) // change this if you want to set a different period - you can set to weeks or months, too. Check the Luxon DateTime API.
+
+  // the date range we want to get information for
+  const dates = {
+    start: then.toISODate(),
+    end: now.toISODate()
+  }
+
+  const retro = await generate(organization, dates)
+  //await generate("listenbourg-legal", dates)
+
+  // write the data out to a file
+  fs.writeFile(path.resolve(`./retros/vbcq-${dates.end}.md`), retro)
+}
+
+markdown()
